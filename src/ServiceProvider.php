aae3cd544981890b513c7d488fb5780bec1fd9d3
Miroslav Borecky <miroslav.borecky@devsk.io>
2026-07-30 14:29:29 +0200

    CP route updated

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -11,6 +11,10 @@ use Statamic\Providers\AddonServiceProvider;
 
 class ServiceProvider extends AddonServiceProvider
 {
+    protected $routes = [
+        'cp' => __DIR__.'/../routes/cp.php',
+    ];
+
     public function register(): void
     {
         parent::register();
